781d2b6b79d2f160d1ca48d4b09865c16fcc042d
RileyKrall <49285110+RileyKrall@users.noreply.github.com>
2019-05-01 10:52:36 -0700

    Update Tweety.java

--- src/toons/Tweety.java
+++ src/toons/Tweety.java
@@ -70,4 +70,20 @@ public class Tweety extends Thread {
     {
         return board.getTweety();
     }
+    
+    public void giveStartingLocation()
+    {
+        Random r = new Random();
+        int x = r.nextInt(4);
+        int y = r.nextInt(4);
+        boolean cont = true;
+        while(cont) {
+            if (board.spaceOccupiedBy(x, y) == 0) {
+                position[0] = x;
+                position[1] = y;
+                board.editSpace(2, x, y);
+                cont = false;
+            }
+        }
+    }
 }
