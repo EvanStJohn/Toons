f0e63956d7d842b4dfd646a6f0d1fbdd7fdd329b
RileyKrall <49285110+RileyKrall@users.noreply.github.com>
2019-05-01 10:52:09 -0700

    Update Marvin.java

--- src/toons/Marvin.java
+++ src/toons/Marvin.java
@@ -65,4 +65,20 @@ public class Marvin extends Thread {
             return false;
         }
     }
+    
+    public void giveStartingLocation()
+    {
+        Random r = new Random();
+        int x = r.nextInt(4);
+        int y = r.nextInt(4);
+        boolean cont = true;
+        while(cont) {
+            if (board.spaceOccupiedBy(x, y) == 0) {
+                position[0] = x;
+                position[1] = y;
+                board.editSpace(4, x, y);
+                cont = false;
+            }
+        }
+    }
 }
