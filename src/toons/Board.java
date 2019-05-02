8216ea4d04d26dec6176b54315a7bd779430ba71
RileyKrall <49285110+RileyKrall@users.noreply.github.com>
2019-05-02 10:05:55 -0700

    Update Board.java

--- src/toons/Board.java
+++ src/toons/Board.java
@@ -83,4 +83,14 @@ public class Board {
             return false;
         }
     }
+    
+    public void printBoard()
+    {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                System.out.print(board[i][j]);
+            }
+            System.out.println();
+        }
+    }
 }
