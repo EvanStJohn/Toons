9784f1658020e826cd6af678b29b0211d033762e
RileyKrall <49285110+RileyKrall@users.noreply.github.com>
2019-05-07 20:05:27 -0700

    Added movement and changed to strings

--- src/toons/Marvin.java
+++ src/toons/Marvin.java
@@ -8,6 +8,7 @@ public class Marvin extends Thread {
     int carrot = 0; // amount of carrots being held
     int[] position = new int[2];
     int turn = 0;
+    String currentPiece = "  M  ";
     Board board;
     
     Marvin(Mutex lock, Board board)
@@ -29,20 +30,23 @@ public class Marvin extends Thread {
                     {
                         lock.wait();
                     }
-                    
-                    // add code below
-                    
-                    if(turn == 3)
-                    {
-                        board.editSpace(0, board.getMountain()[0], board.getMountain()[1]);
-                        board.moveMountain();
-                        turn = 0;
+                    if (board.getWinner() == 0) {
+                       System.out.println("Marvin's turn");
+                        move();
+                        isWinner();  
+                        if(turn == 3)
+                        {
+                            board.editSpace("  -  ", board.getMountain()[0], board.getMountain()[1]);
+                            board.moveMountain();
+                            turn = 0;
+                        }
+                        else
+                        {
+                            turn++;
+                        }
+                        board.printBoard();
                     }
-                    else
-                    {
-                        turn++;
-                    }
-                    
+
                     Thread.sleep(1000);
                     lock.flag = 1;
                     lock.notifyAll();
@@ -69,7 +73,7 @@ public class Marvin extends Thread {
     
     public boolean isWinner()
     {
-        if (carrot == 2 || !board.isLiving())
+        if (carrot == 2 || !board.playersRemaining())
         {
             board.setWinner(4);
             System.out.println("Marvin the Martian is the winner");
@@ -88,12 +92,82 @@ public class Marvin extends Thread {
         while(cont) {
             int x = r.nextInt(5);
             int y = r.nextInt(5);
-            if (board.spaceOccupiedBy(x, y) == 0) {
+            if (board.spaceOccupiedBy(x, y) == "  -  ") {
                 position[0] = x;
                 position[1] = y;
-                board.editSpace(4, x, y);
+                board.editSpace(currentPiece, x, y);
                 cont = false;
             }
         }
     }
+    
+    public void move()
+    {
+        Random rand = new Random();
+        boolean cont = true;
+        while(cont) {
+            int vertOrhoriz = rand.nextInt(2);
+            int newX;
+            int newY;
+            if (vertOrhoriz == 0) {
+                newX = position[0] + (rand.nextInt(3) - 1);
+                newY = position[1];
+            }
+            else {
+                newX = position[0];
+                newY = position[1] + (rand.nextInt(3) - 1);  
+            }
+            
+            String valueOfLocation = board.spaceOccupiedBy(newX, newY);
+            if (valueOfLocation == "  -  ") {
+                board.editSpace("  -  ", position[0], position[1]);
+                position[0] = newX;
+                position[1] = newY;
+                board.editSpace(currentPiece, newX, newY);
+                cont = false;
+            }
+            else if(valueOfLocation == "  C  ") {
+                board.editSpace("  -  ", position[0], position[1]);
+                position[0] = newX;
+                position[1] = newY;
+                board.editSpace(currentPiece, newX, newY);
+                carrot++;
+                cont = false;
+            }
+            
+            else if (valueOfLocation == "  B  " || valueOfLocation == " B(C)") {
+                board.killBugs();
+                board.editSpace("  -  ", position[0], position[1]);
+                position[0] = newX;
+                position[1] = newY;
+                if (valueOfLocation == " B(C)") {
+                    carrot++;
+                    currentPiece = " M(C)";
+                }
+                board.editSpace(currentPiece, newX, newY);
+            }
+            else if (valueOfLocation == "  T  " || valueOfLocation == " T(C)") {
+                board.killTweety();
+                board.editSpace("  -  ", position[0], position[1]);
+                position[0] = newX;
+                position[1] = newY;
+                if (valueOfLocation == " T(C)") {
+                    carrot++;
+                    currentPiece = " M(C)";                    
+                }
+                board.editSpace(currentPiece, newX, newY);
+            }
+            else if (valueOfLocation == "  D  " || valueOfLocation == " D(C)") {
+                board.killTaz();
+                board.editSpace("  -  ", position[0], position[1]);
+                position[0] = newX;
+                position[1] = newY;
+                if (valueOfLocation == " D(C)") {
+                    carrot++;
+                    currentPiece = " M(C)";                    
+                }                
+                board.editSpace(currentPiece, newX, newY);
+            }
+        }
+    } 
 }
