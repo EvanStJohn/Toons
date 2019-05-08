6e89e341e2e93fdc61c2ef150d5dcba075bab54e
RileyKrall <49285110+RileyKrall@users.noreply.github.com>
2019-05-07 20:05:56 -0700

    Changed from ints to strings

--- src/toons/Taz.java
+++ src/toons/Taz.java
@@ -8,6 +8,7 @@ public class Taz extends Thread {
     Mutex lock;
     int carrot = 0; // amount of carrots being held
     int[] position = new int[2];
+    String currentPiece = "  D  ";
     Board board;
     
     Taz(Mutex lock, Board board)
@@ -23,16 +24,19 @@ public class Taz extends Thread {
         {
             synchronized(lock)
             {
-                while (board.getWinner() == 0 && isLiving())
+                while (board.getWinner() == 0 && board.playersRemaining())
                 {
                     while(lock.flag != 3)
                     {
                         lock.wait();
                     }
                     
-                    move();
-                    board.printBoard();
-                    isWinner();
+                    if (this.isLiving() && board.getWinner() == 0) {
+                        System.out.println("Taz's turn");
+                        move();
+                        board.printBoard();
+                        isWinner();
+                    }
                     
                     
                     
@@ -86,10 +90,10 @@ public class Taz extends Thread {
         while(cont) {
             int x = r.nextInt(5);
             int y = r.nextInt(5);
-            if (board.spaceOccupiedBy(x, y) == 0) {
+            if (board.spaceOccupiedBy(x, y) == "  -  ") {
                 position[0] = x;
                 position[1] = y;
-                board.editSpace(3, x, y);
+                board.editSpace(currentPiece, x, y);
                 cont = false;
             }
         }
@@ -113,24 +117,25 @@ public class Taz extends Thread {
                 newY = position[1] + (rand.nextInt(3) - 1);  
             }
             
-            int valueOfLocation = board.spaceOccupiedBy(newX, newY);
-            if (valueOfLocation == 0) {
-                board.editSpace(0, position[0], position[1]);
+            String valueOfLocation = board.spaceOccupiedBy(newX, newY);
+            if (valueOfLocation == "  -  ") {
+                board.editSpace("  -  ", position[0], position[1]);
                 position[0] = newX;
                 position[1] = newY;
-                board.editSpace(3, newX, newY);
+                board.editSpace(currentPiece, newX, newY);
                 cont = false;
             }
-            else if(valueOfLocation == 7 && carrot == 0) {
-                board.editSpace(0, position[0], position[1]);
+            else if(valueOfLocation == "  C  " && carrot == 0) {
+                board.editSpace("  -  ", position[0], position[1]);
                 position[0] = newX;
                 position[1] = newY;
-                board.editSpace(3, newX, newY);
+                currentPiece = " D(C)";
+                board.editSpace(currentPiece, newX, newY);
                 carrot++;
                 cont = false;
             }
-            else if(valueOfLocation == 8 && carrot == 1) {
-                board.editSpace(0, position[0], position[1]);
+            else if(valueOfLocation == "  F  " && carrot == 1) {
+                board.editSpace("  -  ", position[0], position[1]);
                 position[0] = newX;
                 position[1] = newY;
                 cont = false;
